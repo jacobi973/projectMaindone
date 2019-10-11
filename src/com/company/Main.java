e4a46dfcfacae8ff27feebef209cf8eb4b9dbc72
Jacob <jacobit97@hotmail.com>
2019-10-11 13:58:59 -0600

    another i

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Jacob");// write your code here
         System.out.println("Whats up!");
+        int i;
     }
 }
